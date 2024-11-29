23b7668c9abd3222dd109251541c341e3d21d66a
Gabriel ROULEAU <gabriel.rouleau@edu.esiee.fr>
2024-11-29 20:43:47 +0100

    Add env variables to test action

--- config.py
+++ config.py
@@ -3,12 +3,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+LANGUAGES = ["en", "fr"]
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
 database_url = os.environ.get('DATABASE_URL')
 if database_url and database_url.startswith('postgres://'):
     database_url = database_url.replace('postgres://', 'postgresql://')
 
-LANGUAGES = ["en", "fr"]
-SECRET_KEY = os.environ.get('SECRET_KEY')
 SQLALCHEMY_DATABASE_URI = database_url
+
 GOOGLE_CLIENT_ID = os.environ.get('GOOGLE_CLIENT_ID')
 GOOGLE_CLIENT_SECRET = os.environ.get('GOOGLE_CLIENT_SECRET')
