3552eeca9c07d6a43521ce6518a863e524ed7ba9
Gabriel ROULEAU <gabriel.rouleau@edu.esiee.fr>
2024-11-29 15:38:56 +0100

    Fix database url

--- config.py
+++ config.py
@@ -3,8 +3,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+database_url = os.environ.get('DATABASE_URL')
+if database_url.startswith('postgres://'):
+    database_url = database_url.replace('postgres://', 'postgresql://')
+
 LANGUAGES = ["en", "fr"]
 SECRET_KEY = os.environ.get('SECRET_KEY')
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+SQLALCHEMY_DATABASE_URI = database_url
 GOOGLE_CLIENT_ID = os.environ.get('GOOGLE_CLIENT_ID')
 GOOGLE_CLIENT_SECRET = os.environ.get('GOOGLE_CLIENT_SECRET')
